08d1fe5486c86bfa9ad23b9a09e1f891f1f446f0
Ujjwal <uk934010@gmail.com>
2026-06-17 14:29:39 +0530

    Remove cineSage folder

--- cineSage/core.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv
-
-from langchain_mistralai import ChatMistralAI
-
-model = ChatMistralAI(model="mistral-small-latest")
-response = model.invoke("Hello")
-print(response.content)
